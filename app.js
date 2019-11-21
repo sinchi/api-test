e4dbbf09c34b8dd8faad893b2b9fc9e0405070c4
ayoub belghar <ayoub.belghar@capgemini.com>
2019-11-21 09:00:42 +0000

    port

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get('/terrains', (req, res) => {
 
     res.status(200).send(datas);
 });
-
-app.listen(3000, () => {
-    console.log('App listening on port 3000!');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('App listening on port' + PORT);
 });
