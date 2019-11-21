e2046923e1fc37bdc73f59a64a166ebde2cda736
ayoub belghar <ayoub.belghar@capgemini.com>
2019-11-21 09:05:06 +0000

    home

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json());
+app.get("/", (req, res) => {
+    res.status(200).send('test api');
+})
 
 app.get('/terrains', (req, res) => {     
     const datas = [{
